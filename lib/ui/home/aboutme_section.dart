139faf3e3601ae61a4dffd30951fba4b238aafc2
eturkme1 <eturkme1@binghamton.edu>
2024-05-28 14:21:42 -0400

    about me updates

--- lib/ui/home/aboutme_section.dart
+++ lib/ui/home/aboutme_section.dart
@@ -6,112 +6,214 @@ class AboutWidget extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     final isMobile = screenWidth < 600;
-    const double factor = 0.75;
+    final isTablet = screenWidth >= 600 && screenWidth < 1024;
+
+    final double imageWidth = isMobile
+        ? screenWidth * 0.85
+        : isTablet
+            ? screenWidth * 0.75
+            : 526;
+    final double imageHeight = isMobile
+        ? screenHeight * 0.45
+        : isTablet
+            ? screenHeight * 0.55
+            : 380.2;
+
     return Container(
-      width: screenWidth * 0.9 * factor,
-      height: isMobile ? screenHeight * 0.5 * factor : 406.3 * factor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Container(
-            width: isMobile ? screenWidth * 0.9 * factor : 553 * factor,
-            height: isMobile ? screenHeight * 0.5 * factor : 406.3 * factor,
-            child: Stack(
+      width: double.infinity,
+      child: isMobile || isTablet
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Positioned(
-                  top: 26 * factor,
-                  left: 27 * factor,
+                Container(
+                  width: imageWidth / 2,
+                  height: imageHeight / 2,
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        top: isMobile ? 26 : 26,
+                        left: isMobile ? 27 : 27,
+                        child: Container(
+                          width: imageWidth / 2,
+                          height: imageHeight / 2,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(isMobile ? 24 : 24),
+                              topRight: Radius.circular(0),
+                              bottomLeft: Radius.circular(0),
+                              bottomRight: Radius.circular(isMobile ? 24 : 24),
+                            ),
+                            color: Color.fromRGBO(217, 217, 217, 1),
+                            border: Border.all(
+                              color: Color.fromRGBO(255, 255, 255, 1),
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                          width: imageWidth / 2 - 20,
+                          height: imageHeight / 2 - 20,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(isMobile ? 24 : 24),
+                              topRight: Radius.circular(0),
+                              bottomLeft: Radius.circular(0),
+                              bottomRight: Radius.circular(isMobile ? 24 : 24),
+                            ),
+                            image: DecorationImage(
+                              image: AssetImage('lib/assets/ect_logo.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: isMobile ? 20 : 20),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
                   child: Container(
-                    width:
-                        isMobile ? screenWidth * 0.85 * factor : 526 * factor,
-                    height: isMobile
-                        ? screenHeight * 0.45 * factor
-                        : 380.2 * factor,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(24 * factor),
-                        topRight: Radius.circular(0),
-                        bottomLeft: Radius.circular(0),
-                        bottomRight: Radius.circular(24 * factor),
-                      ),
-                      color: Color.fromRGBO(217, 217, 217, 1),
-                      border: Border.all(
-                        color: Color.fromRGBO(255, 255, 255, 1),
-                        width: 2 * factor,
-                      ),
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text(
+                          'ABOUT ME',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: isMobile ? 24 : 24,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        SizedBox(height: isMobile ? 20 : 20),
+                        Text(
+                          'I’m Efe Cankat Turkmen',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontSize: isMobile ? 36 : 36,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        SizedBox(height: isMobile ? 20 : 20),
+                        Text(
+                          'I am an experienced member of the ITS Innovation and Enterprise Applications teams at Binghamton University, specializing in technology solutions and support. My role involves collaborating closely with other team members on a wide range of projects, often working independently or as part of a larger team.',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color.fromRGBO(176, 173, 173, 1),
+                            fontSize: isMobile ? 18 : 18,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
+              ],
+            )
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  width: imageWidth,
+                  height: imageHeight,
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        top: 26,
+                        left: 27,
+                        child: Container(
+                          width: imageWidth,
+                          height: imageHeight,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(24),
+                              topRight: Radius.circular(0),
+                              bottomLeft: Radius.circular(0),
+                              bottomRight: Radius.circular(24),
+                            ),
+                            color: Color.fromRGBO(217, 217, 217, 1),
+                            border: Border.all(
+                              color: Color.fromRGBO(255, 255, 255, 1),
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                          width: imageWidth - 20,
+                          height: imageHeight - 20,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(24),
+                              topRight: Radius.circular(0),
+                              bottomLeft: Radius.circular(0),
+                              bottomRight: Radius.circular(24),
+                            ),
+                            image: DecorationImage(
+                              image: AssetImage('lib/assets/ect_logo.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 Positioned(
-                  top: 0,
-                  left: 0,
+                  top: 21,
+                  left: 600,
                   child: Container(
-                    width:
-                        isMobile ? screenWidth * 0.85 * factor : 526 * factor,
-                    height: isMobile
-                        ? screenHeight * 0.45 * factor
-                        : 380.2 * factor,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(24 * factor),
-                        topRight: Radius.circular(0),
-                        bottomLeft: Radius.circular(0),
-                        bottomRight: Radius.circular(24 * factor),
-                      ),
-                      image: DecorationImage(
-                        image: AssetImage('lib/assets/ect_logo.jpg'),
-                        fit: BoxFit.cover,
-                      ),
+                    width: 580,
+                    height: 366,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'ABOUT ME',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 24,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'I’m Efe Cankat Turkmen',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontSize: 36,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'I am an experienced member of the ITS Innovation and Enterprise Applications teams at Binghamton University, specializing in technology solutions and support. My role involves collaborating closely with other team members on a wide range of projects, often working independently or as part of a larger team.',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Color.fromRGBO(176, 173, 173, 1),
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ],
             ),
-          ),
-          Positioned(
-            top: isMobile ? screenHeight * 0.02 * factor : 21 * factor,
-            left: isMobile ? screenWidth * 0.1 * factor : 600 * factor,
-            child: Container(
-              width: isMobile ? screenWidth * 0.8 * factor : 580 * factor,
-              height: isMobile ? screenHeight * 0.3 * factor : 366 * factor,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'ABOUT ME',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 24 * factor,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  SizedBox(height: 20 * factor),
-                  Text(
-                    'I’m Efe Cankat Turkmen',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 1),
-                      fontSize: 36 * factor,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  SizedBox(height: 20 * factor),
-                  Text(
-                    'I am an experienced member of the ITS Innovation and Enterprise Applications teams at Binghamton University, specializing in technology solutions and support. My role involves collaborating closely with other team members on a wide range of projects, often working independently or as part of a larger team. ',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Color.fromRGBO(176, 173, 173, 1),
-                      fontSize: 18 * factor,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
